12d055fd0602ef4ea7831dc81521345ae4023e8f
tsCalm <dlatkdals93@gmail.com>
2023-11-25 15:10:04 +0900

    feat: typeorm.config

---
+++ src/modules/config/admin.typeorm.config.ts
@@ -0,0 +1,33 @@
+import { ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { join } from 'path';
+
+const prefix = 'LOCAL';
+
+class TypeOrmConfig {
+  static getConfig(configService: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: configService.get(`${prefix}_CRAWL_DB_HOST`),
+      port: configService.get<number>(`${prefix}_CRAWL_DB_PORT`),
+      username: configService.get(`${prefix}_CRAWL_DB_USERNAME`),
+      password: configService.get(`${prefix}_CRAWL_DB_PASSWORD`),
+      database: configService.get(`${prefix}_CRAWL_DB_DATABASE`),
+      entities: [join(__dirname, '../**/crawl/*.entity.js')],
+      synchronize: false,
+      logging: false,
+      autoLoadEntities: false,
+      timezone: 'Z',
+    };
+  }
+}
+
+export const crawlTypeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  name: process.env.CRAWL_DB_CONNECTION_NAME,
+  useFactory: async (configService: ConfigService) =>
+    TypeOrmConfig.getConfig(configService),
+  inject: [ConfigService],
+};
